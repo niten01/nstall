220e465d2b9bed6807e2df97270c07b86b802976
niten <vovcharov@star.intra.ispras.ru>
2025-06-05 17:52:35 +0300

    test num files

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import sys
 from pathlib import Path
@@ -11,12 +12,19 @@ if __name__ == "__main__":
     print("Got args: ", sys.argv)
     repo_root = Path(__file__).parent.parent
     constructor_path = sys.argv[1]
+    dir_to_pack = sys.argv[2]
+    files_to_pack = sum([len(files) for _, _, files in os.walk(dir_to_pack)])
+    print("Files to pack: ", files_to_pack)
     try:
-        subprocess.run([constructor_path, "-n", "a", "-d", repo_root/"src"],
+        subprocess.run([constructor_path, "-n", "a", "-d", dir_to_pack],
                        check=True)
         with tempfile.TemporaryDirectory() as tmp_dir:
             print("Installing to: ", tmp_dir)
             subprocess.run(["./a_Installer", tmp_dir], check=True)
+            extracted_files = sum([len(files)
+                                  for _, _, files in os.walk(tmp_dir)])
+            print("Extracted files: ", extracted_files)
+            assert extracted_files == files_to_pack
     except subprocess.CalledProcessError as e:
         print("Test failed. Output: ")
         print(e)
