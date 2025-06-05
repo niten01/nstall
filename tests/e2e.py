838becfd634076d1ea3367eb3bd15b8409aac9ba
niten <vovcharov@star.intra.ispras.ru>
2025-06-05 16:40:08 +0300

    add CLI only mode

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,8 +1,6 @@
-import os
 import subprocess
 import sys
 from pathlib import Path
-import pathlib
 import tempfile
 
 if __name__ == "__main__":
@@ -10,14 +8,17 @@ if __name__ == "__main__":
         print("Usage test.py <path/to/nstall-constructor> <dir/to/pack>")
         exit(1)
 
+    print("Got args: ", sys.argv)
     repo_root = Path(__file__).parent.parent
     constructor_path = sys.argv[1]
     try:
-        subprocess.run([constructor_path, "-n","a", "-d", repo_root/"src"], check=True)
+        subprocess.run([constructor_path, "-n", "a", "-d", repo_root/"src"],
+                       check=True)
         with tempfile.TemporaryDirectory() as tmp_dir:
             print("Installing to: ", tmp_dir)
             subprocess.run(["./a_Installer", tmp_dir], check=True)
-    except subprocess.CalledProcessError: 
-        print("Test failed")
+    except subprocess.CalledProcessError as e:
+        print("Test failed. Output: ")
+        print(e)
         exit(1)
     print("Test passed")
